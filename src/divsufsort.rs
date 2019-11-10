d48393dccea303e32c84d73792ecf0dd3db7746e
Amos Wenger <amoswenger@gmail.com>
2019-11-10 17:27:59 +0100

    Second block of sort_typeBstar

--- src/divsufsort.rs
+++ src/divsufsort.rs
@@ -1,4 +1,5 @@
 #![allow(nonstandard_style)]
+#![allow(unused)]
 
 pub type Char = u8;
 pub type Idx = i32;
@@ -41,46 +42,120 @@ pub fn divsufsort(T: &[Char], SA: &mut [Idx]) {
     let mut SA = SuffixArray(SA);
 
     // Suffixsort.
-    let res = sort_typeBstar(&T, &mut SA);
+    let _res = sort_typeBstar(&T, &mut SA);
     eprintln!("done enumerating");
 }
 
-#[derive(Debug)]
 struct SortTypeBstarResult {
-    A: Vec<Idx>,
-    B: Vec<Idx>,
+    A: ABucket,
+    B: BMixBucket,
     m: Idx,
+}
+
+struct ABucket(Vec<Idx>);
+
+impl Index<Idx> for ABucket {
+    type Output = Idx;
+
+    fn index(&self, index: Idx) -> &Self::Output {
+        &self.0[index as usize]
+    }
+}
+
+impl IndexMut<Idx> for ABucket {
+    fn index_mut(&mut self, index: Idx) -> &mut Self::Output {
+        &mut self.0[index as usize]
+    }
+}
+
+struct BMixBucket(Vec<Idx>);
+
+impl BMixBucket {
+    #[inline(always)]
+    fn b<'a>(&'a mut self) -> BBucket<'a> {
+        BBucket(&mut self.0)
+    }
+
+    #[inline(always)]
+    fn bstar<'a>(&'a mut self) -> BStarBucket<'a> {
+        BStarBucket(&mut self.0)
+    }
+}
+
+struct BBucket<'a>(&'a mut [Idx]);
+
+impl<'a> Index<(Idx, Idx)> for BBucket<'a> {
+    type Output = Idx;
+
+    fn index(&self, index: (Idx, Idx)) -> &Self::Output {
+        let (c0, c1) = index;
+        &self.0[((c1 << 8) | c0) as usize]
+    }
+}
+
+impl<'a> IndexMut<(Idx, Idx)> for BBucket<'a> {
+    fn index_mut(&mut self, index: (Idx, Idx)) -> &mut Self::Output {
+        let (c0, c1) = index;
+        &mut self.0[((c1 << 8) | c0) as usize]
+    }
+}
+
+struct BStarBucket<'a>(&'a mut [Idx]);
+
+impl<'a> Index<(Idx, Idx)> for BStarBucket<'a> {
+    type Output = Idx;
+
+    fn index(&self, index: (Idx, Idx)) -> &Self::Output {
+        let (c0, c1) = index;
+        &self.0[((c0 << 8) | c1) as usize]
+    }
+}
+
+impl<'a> IndexMut<(Idx, Idx)> for BStarBucket<'a> {
+    fn index_mut(&mut self, index: (Idx, Idx)) -> &mut Self::Output {
+        let (c0, c1) = index;
+        &mut self.0[((c0 << 8) | c1) as usize]
+    }
 }
 
 fn sort_typeBstar(T: &Text, SA: &mut SuffixArray) -> SortTypeBstarResult {
     let n = T.len();
 
     // Initialize bucket arrays
-    let mut A: Vec<Idx> = vec![0; BUCKET_A_SIZE];
-    let mut B: Vec<Idx> = vec![0; BUCKET_B_SIZE];
+    let A: Vec<Idx> = vec![0; BUCKET_A_SIZE];
+    let mut A = ABucket(A);
+
+    let B: Vec<Idx> = vec![0; BUCKET_B_SIZE];
+    let mut B = BMixBucket(B);
 
     // #define BUCKET_A(_c0) bucket_A[(_c0)]
     // #define BUCKET_B(_c0, _c1) (bucket_B[((_c1) << 8) | (_c0)])
     // #define BUCKET_BSTAR(_c0, _c1) (bucket_B[((_c0) << 8) | (_c1)])
 
-    let A_idx = |c0: Char| -> usize { c0 as usize };
-    let B_idx = |c0: Char, c1: Char| -> usize { (((c1 as usize) << 8) | (c0 as usize)) };
-    let BS_idx = |c0: Char, c1: Char| -> usize { (((c0 as usize) << 8) | (c1 as usize)) };
+    // let A_idx = |c0: usize| -> usize { c0 };
+    // let B_idx = |c0: usize, c1: usize| -> usize { ((c1 << 8) | c0) };
+    // let BS_idx = |c0: usize, c1: usize| -> usize { ((c0 << 8) | c1) };
 
-    let mut c1 = 0;
+    // temps
+    let mut c0: Idx;
+    let mut c1: Idx;
+    let mut i: Idx;
+    let mut j: Idx;
+    let mut m: Idx;
+    let mut t: Idx;
 
     // Count the number of occurences of the first one or two characters of each
     // type A, B and B* suffix. Moreover, store the beginning position of all
     // type B* suffixes into the array SA.
-    let mut i = n - 1;
-    let mut m = n;
-    let mut c0 = T[n - 1];
+    i = n - 1;
+    m = n;
+    c0 = T[n - 1] as Idx;
 
     while 0 <= i {
         // type A suffix (originally do..while)
         loop {
             c1 = c0;
-            A[A_idx(c1)] += 1;
+            A[c1] += 1;
 
             // original loop condition
             i -= 1;
@@ -88,7 +163,7 @@ fn sort_typeBstar(T: &Text, SA: &mut SuffixArray) -> SortTypeBstarResult {
                 break;
             }
 
-            c0 = T[i];
+            c0 = T[i] as Idx;
             if !(c0 >= c1) {
                 break;
             }
@@ -96,7 +171,7 @@ fn sort_typeBstar(T: &Text, SA: &mut SuffixArray) -> SortTypeBstarResult {
 
         if 0 <= i {
             // type B* suffix
-            B[BS_idx(c0, c1)] += 1;
+            B.bstar()[(c0, c1)] += 1;
 
             m -= 1;
             SA[m] = i;
@@ -112,13 +187,13 @@ fn sort_typeBstar(T: &Text, SA: &mut SuffixArray) -> SortTypeBstarResult {
                 if i < 0 {
                     break;
                 }
-                c0 = T[i];
+                c0 = T[i] as Idx;
                 if c0 > c1 {
                     break;
                 }
 
                 // body
-                B[B_idx(c0, c1)] += 1;
+                B.b()[(c0, c1)] += 1;
 
                 // iter
                 i -= 1;
@@ -128,7 +203,28 @@ fn sort_typeBstar(T: &Text, SA: &mut SuffixArray) -> SortTypeBstarResult {
     }
     m = n - m;
 
-    for (i, &v) in A.iter().enumerate() {
+    // Note: A type B* suffix is lexicographically smaller than a type B suffix
+    // that beings with the same first two characters.
+
+    // Calculate the index of start/end point of each bucket.
+    {
+        i = 0;
+        j = 0;
+        for c0 in 0..(ALPHABET_SIZE as Idx) {
+            // body
+            t = i + A[c0];
+            A[c0] = i + j; // start point
+            i = t + B.b()[(c0, c0)];
+
+            for c1 in (c0 + 1)..(ALPHABET_SIZE as Idx) {
+                j += B.bstar()[(c0, c1)];
+                B.bstar()[(c0, c1)] = j; // end point
+                i += B.b()[(c0, c1)];
+            }
+        }
+    }
+
+    for (i, &v) in A.0.iter().enumerate() {
         if v == 0 {
             continue;
         }
@@ -136,7 +232,7 @@ fn sort_typeBstar(T: &Text, SA: &mut SuffixArray) -> SortTypeBstarResult {
         eprintln!("A[{:?}] = {}", c, v);
     }
 
-    for (i, &v) in B.iter().enumerate() {
+    for (i, &v) in B.0.iter().enumerate() {
         if v == 0 {
             continue;
         }
@@ -149,9 +245,6 @@ fn sort_typeBstar(T: &Text, SA: &mut SuffixArray) -> SortTypeBstarResult {
         }
         eprintln!("SA[{}] = {}", i, v);
     }
-
-    // Note: A type B* suffix is lexicographically smaller than a type B suffix
-    // that beings with the same first two characters.
 
     // TODO: rest of sort..
 
