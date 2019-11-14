e256fe8b27f9fe38a231f6861e7552653b8d9e0a
Amos Wenger <amoswenger@gmail.com>
2019-11-14 03:28:09 +0100

    Add dc3

---
+++ crates/dc3/src/lib.rs
@@ -0,0 +1,212 @@
+/// Lexicographic order for pairs
+#[inline(always)]
+fn leq2(a1: usize, a2: usize, b1: usize, b2: usize) -> bool {
+    (a1 < b1) || (a1 == b1 && a2 <= b2)
+}
+
+/// Lexicographic order for triples
+#[inline(always)]
+fn leq3(a1: usize, a2: usize, b1: usize, b2: usize, a3: usize, b3: usize) -> bool {
+    (a1 < b1) || (a1 == b1 && leq2(a2, a3, b2, b3))
+}
+
+/// Stably sort a[0..n-1] to b[0..n-1] with keys in 0..K from r
+fn radix_pass(a: &[usize], b: &mut [usize], r: &[usize], n: usize, K: usize) {
+    // counter array
+    let mut c = vec![0_usize; K + 1];
+
+    // count occurrences
+    for i in 0..n {
+        c[r[a[i]]] += 1;
+    }
+
+    // exclusive prefix sums
+    {
+        let mut sum = 0;
+        for i in 0..=K {
+            let t = c[i];
+            c[i] = sum;
+            sum += t;
+        }
+    }
+
+    // sort
+    for i in 0..n {
+        b[c[r[a[i]]]] = a[i];
+        c[r[a[i]]] += 1;
+    }
+}
+
+/// Find the suffix array SA of T[0..n-1] in {1..K}^n
+/// require T[n]=T[n+1]=T[n+2]=0, n >= 2
+#[allow(non_snake_case)]
+pub fn suffix_array(T: &[usize], SA: &mut [usize], n: usize, K: usize) {
+    let n0 = (n + 2) / 3;
+    let n1 = (n + 1) / 3;
+    let n2 = n / 3;
+    let n02 = n0 + n2;
+
+    let mut R = vec![0; n02 + 3];
+    R[n02] = 0;
+    R[n02 + 1] = 0;
+    R[n02 + 2] = 0;
+
+    let mut SA12 = vec![0; n02 + 3];
+    let mut R0 = vec![0; n0];
+    let mut SA0 = vec![0; n0];
+
+    // Step 0: Construct sample
+    // Generate positions of mod 1 and mod 2 suffixes
+    // the "+(n0-n2)" adds a dummy mod 1 suffix if n%3 == 1
+    {
+        let mut j = 0;
+        for i in 0..(n + (n0 - n1)) {
+            if (i % 3) != 0 {
+                R[j] = i;
+                j += 1;
+            }
+        }
+    }
+
+    // Step 1: Sort sample suffixes
+    // lsb radix sort the mod 1 and mod 2 triples
+    radix_pass(&R[..], &mut SA12[..], &T[2..], n02, K);
+    radix_pass(&SA12[..], &mut R[..], &T[1..], n02, K);
+    radix_pass(&R[..], &mut SA12[..], &T[..], n02, K);
+
+    // Find lexicographic names of triples and
+    // write them to the correct places in R
+    let mut name = 0;
+    let mut c0 = 0;
+    let mut c1 = 0;
+    let mut c2 = 0;
+    let mut first = true;
+    for i in 0..n02 {
+        if first || (T[SA12[i]] != c0 || T[SA12[i] + 1] != c1 || T[SA12[i] + 2] != c2) {
+            first = false;
+            name += 1;
+            c0 = T[SA12[i] + 0];
+            c1 = T[SA12[i] + 1];
+            c2 = T[SA12[i] + 2];
+        }
+        if SA12[i] % 3 == 1 {
+            // write to R1
+            R[SA12[i] / 3] = name;
+        } else {
+            // write to R2
+            R[SA12[i] / 3 + n0] = name;
+        }
+    }
+
+    // recurse if names are not yet unique
+    if name < n02 {
+        suffix_array(&R[..], &mut SA12[..], n02, name);
+        // store unique names in R using the suffix array
+        for i in 0..n02 {
+            R[SA12[i]] = i + 1;
+        }
+    } else {
+        // generate the suffix array of R directly
+        for i in 0..n02 {
+            SA12[R[i] - 1] = i;
+        }
+    }
+
+    // Step 2: sort nonsample suffixes
+    // stably sort the mod 0 suffixes from SA12 by their first character
+    {
+        let mut j = 0;
+        for i in 0..n02 {
+            if SA12[i] < n0 {
+                R0[j] = 3 * SA12[i];
+                j += 1;
+            }
+        }
+        radix_pass(&R0[..], &mut SA0[..], T, n0, K);
+    }
+
+    // Step 3: merge
+    // merge sorted SA0 suffixes and sorted SA12 suffixes
+    {
+        let mut p = 0;
+        let mut t = n0 - n1;
+        let mut k = 0;
+        while k < n {
+            macro_rules! get_i {
+                () => {
+                    if SA12[t] < n0 {
+                        SA12[t] * 3 + 1
+                    } else {
+                        (SA12[t] - n0) * 3 + 2
+                    }
+                };
+            }
+
+            // pos of current offset 12 suffix
+            let i = get_i!();
+            // pos of current offset 0 suffix
+            let j = SA0[p];
+
+            let sa12_smaller = if SA12[t] < n0 {
+                leq2(T[i], R[SA12[t] + n0], T[j], R[j / 3])
+            } else {
+                leq3(
+                    T[i],
+                    T[i + 1],
+                    R[SA12[t] - n0 + 1],
+                    T[j],
+                    T[j + 1],
+                    R[j / 3 + n0],
+                )
+            };
+            if sa12_smaller {
+                // suffix from SA12 is smaller
+                SA[k] = i;
+                t += 1;
+                if t == n02 {
+                    // done --- only SA0 suffixes left
+                    k += 1;
+                    while p < n0 {
+                        SA[k] = SA0[p];
+                        p += 1;
+                        k += 1;
+                    }
+                }
+            } else {
+                // suffix from SA0 is smaller
+                SA[k] = j;
+                p += 1;
+                if p == n0 {
+                    // done ---- only SA12 suffixes left
+                    k += 1;
+                    while t < n02 {
+                        SA[k] = get_i!();
+                        t += 1;
+                        k += 1;
+                    }
+                }
+            }
+            k += 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let T = vec![6, 3, 1, 9, 12, 30, 0, 0, 0];
+        let n = T.len() - 3;
+        let mut SA = vec![0; n];
+        let K = *T.iter().max().unwrap();
+
+        suffix_array(&T[..], &mut SA[..], n, K);
+        println!("T = {:?}", T);
+        println!("SA = {:?}", SA);
+        for i in 0..(n - 1) {
+            assert!(T[SA[i]..] < T[SA[i + 1]..])
+        }
+    }
+}
